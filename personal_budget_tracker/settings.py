e06b541983fadc4e8486bdd505b8dd4252aa85a5
Mateusz Kuniczuk <mateusz.kuniczuk98@gmail.com>
2025-04-16 23:22:59 +0200

    postgres setup

--- personal_budget_tracker/settings.py
+++ personal_budget_tracker/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'personal_budget_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'budget_db',
+        'USER': 'postgres',
+        'PASSWORD': 'jeff',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
